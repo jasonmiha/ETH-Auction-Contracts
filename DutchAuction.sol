831cf7cedef7971b745167699e371f2ce0b69203
Kevin Li <kli74@jhu.edu>
2024-10-14 14:59:22 -0400

    implement 23/27

--- DutchAuction.sol
+++ DutchAuction.sol
@@ -4,25 +4,73 @@ pragma solidity ^0.8.0;
 import "./Assignment2.sol";
 
 contract DutchAuction is IDutchAuction {
-    address public seller;
+    address public override seller;
+    address public override winner;
+    uint256 public override finalPrice;
 
-    address public winner;
-    // function winner() public view returns (address) {}
+    uint256 private initialPrice;
+    uint256 private blockDecrement;
+    uint256 private endBlock;
+    uint256 private startBlock;
 
-    uint256 public finalPrice;
-    // function finalPrice() public view returns (uint256) {}
+    bool private finalized;
 
     // Note: Contract creator should be the seller
     constructor(
         uint256 _initialPrice,
         uint256 _blockDecrement,
         uint256 _duration
-    ) {}
+    ) {
+        require(_initialPrice > 0, "Initial price must be greater than 0");
+        require(_blockDecrement > 0, "Block decrement must be greater than 0");
+        require(_duration > 0, "Duration must be greater than 0");
 
-    function bid() external payable override {}
+        seller = msg.sender;
+        initialPrice = _initialPrice;
+        blockDecrement = _blockDecrement;
+        startBlock = block.number;
+        endBlock = startBlock + _duration;
+    }
+
+    function bid() external payable override {
+        require(block.number <= endBlock, "Auction has ended");
+        require(winner == address(0), "Auction already has a winner");
+
+        uint256 price = currentPrice();
+        require(msg.value >= price, "Bid is too low");
+
+        winner = msg.sender;
+        finalPrice = price;
+
+        if (msg.value > price) {
+            payable(msg.sender).transfer(msg.value - price);
+        }
+    }
 
     // Anyone can finalize the auction after the auction has ended
-    function finalize() external override {}
+    function finalize() external override {
+        require(block.number > endBlock || winner != address(0), "Auction not yet ended");
+        require(!finalized, "Auction already finalized");
 
-    function currentPrice() public view override returns (uint256) {}
+        finalized = true;
+
+        if (winner != address(0)) {
+            payable(seller).transfer(finalPrice);
+        }
+    }
+
+    function currentPrice() public view override returns (uint256) {
+        if (block.number >= endBlock) {
+            return 0;
+        }
+
+        uint256 elapsedBlocks = block.number - startBlock;
+        uint256 totalDecrement = elapsedBlocks * blockDecrement;
+
+        if (totalDecrement >= initialPrice) {
+            return 0;
+        }
+
+        return initialPrice - totalDecrement;
+    }
 }
